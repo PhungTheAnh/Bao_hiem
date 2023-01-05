57791bbb1a48c9867b39d22d4ed66d61b271cd5f
Phung The Anh <phungtheanh2001@gmail.com>
2023-01-05 15:01:59 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const path = require("path");
 const express = require("express");
 const handlebars = require("express-handlebars");
-const app = express();  
+const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const route = require("./routes/index");
+const mysql = require("mysql");
 
 // http logger
 // app.use(morgan("combined"));
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+//
 app.use(express.static(path.join(__dirname, "public")));
 // Template engine
 app.engine(
@@ -19,9 +31,9 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-
 // Route init
 route(app);
+
 
 app.listen(port, () =>
   console.log(`Du an dang chay tai http://localhost:${port}`)
