27b138925c80c0317a064400a8d68a66ffd77951
Phung The Anh <phungtheanh2001@gmail.com>
2023-01-06 17:55:54 +0700

    2

--- src/routes/bao-gia copy 2/bao-gia.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const BaogiaController = require("../../app/controllers/layout/baoGiaController");
-//
-router.use("/", BaogiaController.index);
-
-module.exports = router;
